2840f1bb1375a55ce6e137045eaacdfbaf631d04
Vladimir M <vovencia@gmail.com>
2018-12-07 14:58:47 +0300

    applyAppMIddleware

--- WeatherStation-backend/src/index.ts
+++ WeatherStation-backend/src/index.ts
@@ -10,33 +10,29 @@ const apiPort = /*@if !API_PORT*/3001/*@endif*/ /*@echo API_PORT*/;
 
 let app;
 
-if(isMainModule){
-	app = express();
-	
+const applyAppMIddleware = (app) => {
 	app.use(function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		next();
 	});
-	
+
 	app.use(helmet());
 	app.use(shield());
 	app.use(api);
+}
+
+if(isMainModule){
+	app = express();
+
+	applyAppMIddleware(app);
 	
 	app.listen(apiPort);
 
 } else {
 	app = express.Router();
 
-	app.use(function(req, res, next) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		next();
-	});
-
-	app.use(helmet());
-	app.use(shield());
-	app.use(api);
+	applyAppMIddleware(app);
 }
 
 export default app;
